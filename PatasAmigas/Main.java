58c89f00d67ec9730a4e85b627c89f0746bb4274
MateusKekw <mendonca.mateus@aluno.ifsp.edu.br>
2024-10-04 17:42:38 -0300

    Mudanças na classe Pessoa

--- PatasAmigas/Main.java
+++ PatasAmigas/Main.java
@@ -4,14 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        Pessoa pessoa = new Pessoa();
+        Pessoa pessoa = new Pessoa();
 
         System.out.println("Bem-vindo ao sistema de gerenciamento de Pessoas!");
 
         boolean running = true;
         while (running) {
             System.out.println("\nEscolha uma opção:");
-            System.out.println("1. Criar nova pessoa");
+            System.out.println("1. Criar novo Usuário");
             System.out.println("2. Exibir informações da pessoa");
             System.out.println("3. Editar informações da pessoa");
             System.out.println("4. Sair");
@@ -19,7 +19,7 @@ public class Main {
             int choice = scanner.nextInt();
             scanner.nextLine();
 
-/*             switch (choice) {
+            switch (choice) {
                 case 1:
                     pessoa = criarPessoa(scanner);
                     break;
@@ -35,58 +35,17 @@ public class Main {
                     break;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
-            }*/
+            
         }
 
         scanner.close();
     }
 
-/*     private static Pessoa criarPessoa(Scanner scanner) {
-        System.out.println("\n--- Criar Nova Pessoa ---");
 
-        System.out.print("Nome: ");
-        String nome = scanner.nextLine();
-
-        System.out.print("Sobrenome: ");
-        String sobrenome = scanner.nextLine();
-
-        System.out.print("Email: ");
-        String email = scanner.nextLine();
-
-        System.out.print("Logradouro: ");
-        String logradouro = scanner.nextLine();
-
-        System.out.print("Número: ");
-        String numero = scanner.nextLine();
-
-        System.out.print("Bairro: ");
-        String bairro = scanner.nextLine();
-
-        System.out.print("Cidade: ");
-        String cidade = scanner.nextLine();
-
-        System.out.print("Estado: ");
-        String estado = scanner.nextLine();
-
-        System.out.print("País: ");
-        String pais = scanner.nextLine();
-
-        System.out.print("Nacionalidade: ");
-        String nacionalidade = scanner.nextLine();
-
-        // Você pode definir o ID da pessoa conforme necessário (ex: auto-incremento ou geração manual)
-        int idPessoa = 1; // Exemplo de ID fixo
-
-//        Pessoa novaPessoa = new Pessoa(idPessoa, nome, sobrenome, email, logradouro, numero, bairro, cidade, estado, pais, nacionalidade);
-
-        System.out.println("Pessoa criada com sucesso!");
-        return novaPessoa;
-    }*/
-
-    private static void exibirPessoa(Pessoa pessoa) {
-        System.out.println("\n--- Informações da Pessoa ---");
-        System.out.println(pessoa.toString());
-    }
+    //private static void exibirPessoa(Pessoa pessoa) {
+    //    System.out.println("\n--- Informações da Pessoa ---");
+    //    System.out.println(pessoa.toString());
+    //}
 
     /*private static void editarPessoa(Scanner scanner, Pessoa pessoa) {
         System.out.println("\n--- Editar Pessoa ---");
@@ -133,4 +92,5 @@ public class Main {
 
         System.out.println("Informações da pessoa atualizadas com sucesso!");
     }*/
+}
 }
--- PatasAmigas/Pessoa.java
+++ PatasAmigas/Pessoa.java
@@ -1,82 +1,156 @@
-public abstract class Pessoa {
+import java.util.Scanner;
 
-private int ID;
-private static int IDGenerator = 0;
-private String CPF;
-private String dataDeNascimento;
-private String email;
-private String endereco;
-private String nome;
-private String senha;
-private String telefone;
+public class Pessoa {
 
-public Pessoa(String CPF, String endereco, String nome, String dataDeNascimento, String email, String senha, String telefone){
+    private int ID;
+    private static int IDGenerator = 0;
+    private String CPF;
+    private String dataDeNascimento;
+    private String email;
+    private String endereco;
+    private String nome;
+    private String senha;
+    private String telefone;
+    private boolean isAdotante;
+    private boolean isTutor;
+    private boolean isFuncionario; 
 
-    setID();
-    setCPF(CPF);
-    setEndereco(endereco);
-    setNome(nome);
-    setDataNascimento(dataDeNascimento);
-    setEmail(email);
-    setSenha(senha);
-    setTelefone(telefone);
-}
+    public Pessoa(String CPF, String endereco, String nome, String dataDeNascimento, String email, String senha, String telefone, boolean isAdotante, boolean isTutor, boolean isFuncionario){
 
-private void setID(){
-    this.ID = IDGenerator++;
-}
-public String getCPF(){
-    return CPF;
-}
-public void setCPF(String CPF){
-    this.CPF = CPF;
-}
-public String getEndereco(){
-    return endereco;
-}
-public void setEndereco(String endereco){
-    this.endereco = endereco;
-}
-public String getNome(){
-    return nome;
-}
-public void setNome(String nome){
-    this.nome = nome;
-}
-public String getDataDeNascimento(){
-    return getDataDeNascimento();
-}
-public void setDataNascimento(String dataDeNascimento){
-    this.dataDeNascimento = dataDeNascimento;
-}
-public String getEmail(){
-    return email;
-}
-public void setEmail(String email){
-    this.email = email;
-}
-public String getSenha(){
-    return senha;
-}
-public void setSenha(String senha){
-    this.senha = senha;
-}
-public String getTelefone(){
-    return telefone;
-}
-public void setTelefone(String telefone){
-    this.telefone = telefone;
-}
-@Override
-public String toString(){
-    return "Pessoa{"+
-    "ID="+ ID +
-    ", CPF='" + CPF+ '\'' +
-    ", dataDeNascimento='" + dataDeNascimento + '\'' +
-    ", nome='" + nome + '\'' +
-    ", senha='" + senha + '\'' +
-    ", telefone='" + telefone + '\'' +
-    '}';
-}
+        setID();
+        setCPF(CPF);
+        setEndereco(endereco);
+        setNome(nome);
+        setDataNascimento(dataDeNascimento);
+        setEmail(email);
+        setSenha(senha);
+        setTelefone(telefone);
+        setIsAdotante(isAdotante);
+        setIsTutor(isTutor);
+        setIsFuncionario(isFuncionario);
+    }
+
+    private void setID(){
+        this.ID = IDGenerator++;
+    }
+    public String getCPF(){
+        return CPF;
+    }
+    public void setCPF(String CPF){
+        this.CPF = CPF;
+    }
+    public String getEndereco(){
+        return endereco;
+    }
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getDataDeNascimento(){
+        return dataDeNascimento;
+    }
+    public void setDataNascimento(String dataDeNascimento){
+        this.dataDeNascimento = dataDeNascimento;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getSenha(){
+        return senha;
+    }
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+    public String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;}
+
+    public boolean getIsAdotante(){
+        return isAdotante;
+    }
+
+    public void setIsAdotante(boolean isAdotante){
+        this.isAdotante = isAdotante;
+    }
+
+    public boolean getIsTutor(){
+        return isTutor;
+    }
+
+    public void setIsTutor(boolean isTutor){
+        this.isTutor = isTutor;
+    }
+
+    public boolean getIsFuncionario(){
+        return isFuncionario;
+    }
+
+    public void setIsFuncionario(boolean isFuncionario){
+        this.isFuncionario = isFuncionario;
+    }
+
+    @Override
+    public String toString(){
+        return "Pessoa{"+
+        "ID="+ ID +
+        ", CPF='" + CPF+ '\'' +
+        ", dataDeNascimento='" + dataDeNascimento + '\'' +
+        ", nome='" + nome + '\'' +
+        ", senha='" + senha + '\'' +
+        ", telefone='" + telefone + '\'' +
+        '}';
+    }
+
+
+    private Pessoa criarPessoa(Scanner scanner) {
+            System.out.println("\n--- Criar Nova Pessoa ---");
+
+            System.out.print("CPF: ");
+            String cpf = scanner.nextLine();
+
+            System.out.print("Data de Nascimento: ");
+            String dataNascimento = scanner.nextLine();
+
+            System.out.print("E-mail: ");
+            String email = scanner.nextLine();
+
+            System.out.print("Endereço: ");
+            String endereco = scanner.nextLine();
+
+            System.out.print("Nome: ");
+            String nome = scanner.nextLine();
+
+            System.out.print("Senha: ");
+            String senha = scanner.nextLine();
+
+            System.out.print("Telefone: ");
+            String telefone = scanner.nextLine();
+
+
+            System.out.print("Você é um Adotante?");
+            boolean isAdotante = scanner.hasNext();
+
+            System.out.print("Você é um Tutor?");
+            boolean isTutor = scanner.hasNext();
+
+            System.out.print("Você é um Funcionário?");
+            boolean isFuncionario = scanner.hasNext();
+
+
+            Pessoa novaPessoa = new Pessoa(cpf, dataNascimento, email, endereco, nome, senha, telefone, isAdotante, isTutor, isFuncionario);
+
+            System.out.println("Pessoa criada com sucesso!");
+            return novaPessoa;
+        }
 
 }
